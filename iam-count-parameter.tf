250ebbc9f3857e572592723d3a6c8d31d1371f66
Gaurav Gadbail <45719606+GauravGadbail26@users.noreply.github.com>
2023-02-06 10:53:52 +0100

    Update iam-count-parameter.tf

--- iam-count-parameter.tf
+++ iam-count-parameter.tf
@@ -1,3 +1,7 @@
+## This snippet is from the Count and Count Index video.
+
+### iam-count-parameter.tf
+
 provider "aws" {
   region     = "us-west-2"
   access_key = "YOUR-ACCESS-KEY"
@@ -14,3 +18,21 @@ resource "aws_iam_user" "lb" {
   count = 3
   path = "/system/"
 }
+
+=================================================
+### count-paremeter.tf
+
+```sh
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "YOUR-ACCESS-KEY"
+  secret_key = "YOUR-SECRET-KEY"
+}
+
+
+resource "aws_instance" "instance-1" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+   count = 3
+}
+```
